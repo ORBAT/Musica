d528d9f4dc95edcc3136ecd7e5c98c240e7ec85e
Tom Eklöf <tomeklof@gmail.com>
2023-04-17 22:53:16 +0300

    lisää siivousta
    CANeuronin foldauksen ja tyyppien mehustelua

--- src/Neuron.jl
+++ src/Neuron.jl
@@ -21,32 +21,33 @@ using TestItems, Test, Lazy
 
 # @from "CA.jl" import DiscreteCA
 
-struct CAN1{InWidth,OutWidth,NStates}
+abstract type Neuron{InWidth, OutWidth, NStates} end
+
+struct CAN1{Width,NStates} <: Neuron{Width,Width,NStates}
   ca::DiscreteCA
   generations::Int
-  inWidth::Int
-  outWidth::Int
+  width::Int
   nStates::Int
 
-  function CAN1{InWidth,OutWidth,NStates}(ca::DiscreteCA{NStates,Radius,RuleLen}, gens::Int) where {InWidth,OutWidth,NStates,Radius,RuleLen}
-    new(ca, gens, InWidth, OutWidth, NStates)
+  function CAN1{Width,NStates}(ca::DiscreteCA{NStates,Radius,RuleLen}, gens::Int) where {Width,NStates,Radius,RuleLen}
+    new(ca, gens, Width, NStates)
   end
 
-  function CAN1{Width,NStates}(ca::DiscreteCA{NStates,Radius,RuleLen}, gens::Int) where {Width,NStates,Radius,RuleLen}
-    new{Width,Width,NStates}(ca, gens, Width, Width, NStates)
-  end
 end
 
 CANeuron = CAN1
 
+_left(a,b) = a
+
 function (can::CANeuron{W,N})(state::State)::State where {W,N,State}
-  foldl(1:(can.generations); init = state) do acc, _
-    can.ca(acc)
-  end
+  # foldl takes a fn (acc, x). (can.ca ∘ _left) is (acc,_) -> can.ca(acc)
+  # this is basically just can.ca composed with itself can.generation times (can.ca ∘ can.ca ∘ ... )
+  foldl((can.ca ∘ _left), 1:can.generations; init = state)
 end
 
 @testitem "CA neurons" begin
   @test CANeuron{4,2}(DiscreteCA{2,1}(110), 3)([1, 0, 0, 0]) == [1, 0, 1, 1]
+  @inferred CANeuron{4,2}(DiscreteCA{2,1}(110), 3)([1, 0, 0, 0])
 end
 
 # struct 
