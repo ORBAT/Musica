6ef5d6e243b66a7f19e0c363c8f0f1cb2f7ab6f6
Tom Eklöf <tomeklof@gmail.com>
2023-04-19 00:14:59 +0300

    startup-skripta

--- startup.jl
+++ startup.jl
@@ -1,12 +1,17 @@
-using TestItems, Test, Transducers, StaticArrays, Evolutionary, Revise
+using TestItems, Test, Transducers, StaticArrays, Evolutionary, Revise, BenchmarkTools
 
 includet("src/Musica.jl")
 ##includet("src/CA.jl")
 
 function new_state(row_len)
-  @SVector ones(Int, row_len)
+  state = let bla = @MVector zeros(Int, row_len)
+    bla[1] = 1
+    SVector(bla)
+  end
 end
 
 test_state = new_state(32)
 
 test_ca = Musica.DiscreteCA{2,1}(110)
+
+r=Musica.Row{2}(@SVector [1,2,3,4])
