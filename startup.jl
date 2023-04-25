a0630752e007b22d94e7640e0c413d28854ff967
Tom Eklöf <tomeklof@gmail.com>
2023-04-25 15:50:50 +0300

    parempi startup.jl

--- startup.jl
+++ startup.jl
@@ -1,18 +1,32 @@
 using TestItems, Test, Transducers, StaticArrays, Evolutionary, Revise, BenchmarkTools
 
+BenchmarkTools.DEFAULT_PARAMETERS.samples = 30000
+
 includet("src/Musica.jl")
 ##includet("src/CA.jl")
 
-function new_state(row_len)
-  let bla = @MVector zeros(Int, row_len)
+using .Musica
+
+function new_state(::Type{Val{L}})::SizedVector{L} where {L}
+  state = let bla = zeros(Int, L)
     bla[1] = 1
-    SVector(bla)
+    SizedVector{L}(bla)
   end
 end
 
-test_state = Musica.Row{2}(new_state(8))
+function new_state(::Val{L})::SizedVector{L} where {L}
+  new_state(Val{L})
+end
 
-test_ca = Musica.DiscreteCA{2}(110)
-test_ca2 = Musica.DiscreteCA{2,1}(54)
+new_state(v::Integer) = new_state(Val(v))
+
+test_ca = DiscreteCA{2,1}(110)
+test_state=Row{2}(new_state(Val{32}))
+n_generations=40
+
+
+
+test_ca = DiscreteCA{2}(110)
+test_ca2 = DiscreteCA{2,1}(54)
 
 # r=Musica.Row{2}(@SVector [1,2,3,4])
