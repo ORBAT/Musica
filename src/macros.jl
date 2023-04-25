5d475ed577383d44c03065b05b9bd6b90cd1e128
Tom Eklöf <tomeklof@gmail.com>
2023-04-25 15:50:27 +0300

    bleb

--- src/macros.jl
+++ src/macros.jl
@@ -11,14 +11,14 @@ Useful for eg. defining new subtypes of `AbstractArray`
 
     @forward MyArray.coll Base.size, Base.getindex, Base.setindex!
 
-Modified from [MacroTools'](https://github.com/FluxML/MacroTools.jl) macro by dropping the @inbounds annotation from the head of the function
-body. Based on some benchmarks it makes things a bit slower
+Modified from [MacroTools'](https://github.com/FluxML/MacroTools.jl) macro by using the `Base.@propagate_inbounds` macro instead of the internal
+`Base.@_propagate_inbounds_meta` macro
 """
 macro forward(ex, fs)
   @capture(ex, T_.field_) || error("Syntax: @forward T.x f, g, h")
   T = esc(T)
   fs = isexpr(fs, :tuple) ? map(esc, fs.args) : [esc(fs)]
-  :($([:($f(x::$T, args...; kwargs...) = ($f(x.$field, args...; kwargs...)))
+  :($([:(Base.@propagate_inbounds $f(x::$T, args...; kwargs...) = ($f(x.$field, args...; kwargs...)))
        for f in fs]...);
     nothing)
 end
