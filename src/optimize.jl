12663ef8ad641626f257c2b3e49b9d9e0d73109c
Tom Eklöf <tomeklof@gmail.com>
2023-05-30 18:02:15 +0300

    optimize.jl:n uusiks mietintää + skräts

--- src/optimize.jl
+++ src/optimize.jl
@@ -21,7 +21,7 @@ julia> Musica._normalize([5 5 5])
 """
 function _normalize(x)
   (_min, _max) = extrema(x)
-  if _min == _max == 0
+  if 0 == _min == _max
     return zeros(Float64, size(x))
   end
   _diff = _max - _min
@@ -32,92 +32,60 @@ function _normalize(x)
   end
 end
 
-@inline _normalize_num(n, max_val) = n / max_val
-@inline _normalize_num(n, ::Type{Val{max_val}}) where {max_val} = _normalize_num(n, max_val)
-@inline _normalize_num(n, ::Val{max_val}) where {max_val} = _normalize_num(n, max_val)
-"""
-TODO: enkuksi
-
-
-
-Palauttaa tavoitefunktion, joka ottaa raakabittejä (tmv) ja
-- parsii yksilön `indiv` fenotyypin biteistä `genotype_parser`:illa
-- ajaa `fitness_fn(indiv)`
-
-Tyypit:
-- `indiv::row -> row` (eli _ainakin_ tyyppi joka käyttäytyy ku funktio `row -> row`)
-- `fitness_fn::indiv -> Float64`. EI `row` koska jatkossa voi haluta että esim. genomin pituus tmv vaikuttaa fitnessiin
-- `genotype_parser::genome -> indiv`
-- paluuarvo: genome -> Float64
-"""
-@inline function to_obj_fn(genotype_parser::Function, fitness_fn::Function)
-  # HOX: DEPREKOITU
-  error("ÄLÄ KÄYTÄ")
-  function obj_fn(genome)
-    # _ on genome:istä consumeamatta jääneet bitit
-    (_, indiv) = genome |> genotype_parser
-    indiv |> fitness_fn
-  end
-end
-
-"""
-- `result_generator = indiv::Fn -> (wanted::Number[], Vector{Row})`. Ottaa yksilön, ajaa sen läpi jotain inputtia ja pullauttaa ulos sekä , että resultin eli listan rivejä. Luodaan funkkarilla jonka tyyppi on `input_data -> indiv -> Vector{Row}` (`input_data` ei välttis ole sama kuin `wanted`)
-- `fitness_fn = (wanted::Number[], )`
-"""
-@inline function to_obj_fn(genotype_parser_fn, result_generator_fn, fitness_fn)
-  function obj_fn(bits)
-    # HOX: tässä mallissa result_generator_fn:n ja fitness_fn:n välillä on riippuvuus. Esim se samplaava obj fun, jossa StatsBase.rmsd(result, wanted[idxs])
-    # TODO: tää ratkasu että result_generator_fn palauttaa myös wanted-Arrayn on kyllä vähän vitusta, mutta menköön ny toistaseks
-    res = (bits |> genotype_parser_fn |> _right |> result_generator_fn)
-    fitness_fn(res...)
-  end
-end
-
-gray_inp_out(w::Type{Val{width}}=Val{16}) where {width} = @£(num_to_gray_row(w)), (@£(_normalize_num(2^width-1)) ∘ row_from_gray)
-
-function full_result_generator(wanted::T; state_bits=16, mappers=gray_inp_out(Val{state_bits})) where {T<:AbstractArray}
-  (input_mapper, output_mapper) = mappers
-  len_wanted = length(wanted)
-  inp = 1:len_wanted |> Map(input_mapper)
-  function full_result(indiv)
-    # @info "full_result indiv", indiv
-    # @info "full res inp", typeof(inp)
-    result = inp |> Map(output_mapper ∘ indiv) |> collect
-    # any(map(row -> !all(==(0), row), result)) && @info "nonzero " result
-    # error("HUORA")
-    (wanted, result)
-  end
-end
-
-"""
-- `input_mapper` muuntaa inputin, joka on luultavasti joku Number[], `Row`iksi jollain enkoodauksella (esim. Gray [num_to_gray_row](@ref))
-- `output_mapper` sama ku ^ mutta toisin päin, Row -> Number
-"""
-function sampled_result_generator(wanted::T; state_bits=16, sequence_len=3, n_samples=6, mappers=gray_inp_out(Val{state_bits})) where {T<:AbstractArray}
-  (input_mapper, output_mapper) = mappers
-  start_idx_range = 1:(length(wanted)-sequence_len)
-  _n_samples = min(length(start_idx_range), n_samples)
-  to_ranges = Map(start_idx -> start_idx:start_idx+sequence_len) |> Cat() |> Unique()
-  function sampled_result(indiv)
-    start_idxs = rand(start_idx_range, _n_samples)
-    idxs = start_idxs |> to_ranges |> collect
-    inp = idxs |> Map(input_mapper)
-    result = inp |> Map(output_mapper ∘ indiv) |> collect
-    (wanted[idxs], result)
-  end
-end
-
-@inline function make_fitness_function(stats_fn::Function=StatsBase.rmsd)
-  function fitness_fn(wanted, result)
-    stats_fn(wanted, result)
-  end
-end
-
 
 @inline _right((_, r)) = r
 @inline _right(_, r) = r
 
-@testitem "to_obj_fn(gen_parse_fn, res_gen_fn, fit_fn)" begin
+@inline _normalize_num(n, max_val) = n / max_val
+
+function create_fitness_fn(fn=StatsBase.rmsd, output_mapper=@£(_normalize_num(2^16-1)) ∘ row_from_gray)
+  function fitness_fn(wanted, result)
+    _wanted = wanted |> maybe_collect
+    _result = result |> Map(output_mapper) |> maybe_collect
+    fn(_wanted, _result)
+  end
+end
+
+@inline function create_obj_fn(genotype_parser_fn, input_data_gen_fn, result_gen_fn, fitness_fn)
+  return function obj_fn(bits)
+    (wanted, input) = input_data_gen_fn()
+    # HUOM: _ on ylijääneet bitit
+    (_, indiv) = bits |> genotype_parser_fn
+    result = result_gen_fn(input, indiv)
+    fitness_fn(wanted, result)
+  end
+end
+
+# gray_inp_out(w::Type{Val{width}}=Val{16}) where {width} = @£(num_to_gray_row(w)), (@£(_normalize_num(2^width - 1)) ∘ row_from_gray)
+
+function input_based_result_gen(input_mapper=num_to_gray_row(Val{16}))
+  function _input_based_result_gen(input, indiv)
+    input |> Map(indiv ∘ input_mapper)
+  end
+end
+
+function full_data_generator(wanted::T) where {T<:AbstractArray}
+  function _full_data_generator()
+    (wanted, 1:length(wanted))
+  end
+end
+
+using Random
+
+function sampled_data_generator(wanted::T; sequence_len=3, n_samples=6, rng=Random.default_rng()) where {T<:AbstractArray}
+  start_idx_range = 1:(length(wanted)-(sequence_len-1))
+  _n_samples = min(length(start_idx_range), n_samples)
+  to_ranges = MapCat(start_idx -> start_idx:start_idx+(sequence_len-1)) |> Unique()
+  # NOTE: palauttaa (wanted, input)
+  function _sampled_data_generator()
+    start_idxs = rand(rng, start_idx_range, _n_samples)
+    idxs = start_idxs |> to_ranges
+    ((wanted,) |> MapCat(w -> w[idxs |> collect]), idxs)
+  end
+end
+
+
+#= @testitem "to_obj_fn(gen_parse_fn, res_gen_fn, fit_fn)" begin
   using StaticArrays
   indiv_bits = digits(110; base=2, pad=8)
   _ca = Musica.DiscreteCA{2}(110)
@@ -130,19 +98,17 @@ end
     end
   end
 
-  function _res_gen(indiv)
-    (_wanted, [indiv(Row{2}(@SVector [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]))])
+  function _res_gen(input, indiv)
+    indiv(input)
   end
 
   function _fit_fn(wanted, rows)
     rows == [wanted] ? 0.0 : 1.0
   end
 
-  @test Musica.to_obj_fn(_parserfn, _res_gen, _fit_fn)(indiv_bits) == 0
-
   _TestStack = Musica.CANeuronStack{1,2,16}
   _test_parser = Musica.parser(_TestStack; bits_per_gen=2)
-  
+
 
   #=   state = Row{2}(@SVector [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
   ca = DiscreteCA{2}(110)
@@ -151,61 +117,7 @@ end
   @inferred(ca(state)) =#
 
 end
-
-function gray_seq_sample_fitness_fn(wanted::T; state_bits::Int=16, sequence_len=4) where {T<:AbstractArray}
-  start_idx = rand(1:length(wanted)-sequence_len)
-
-  idxs = start_idx:start_idx+sequence_len
-  inp = idxs |> Map(num_to_gray_row)
-  state_max_val = 2^state_bits - 1
-  _normalize_output = @£ _normalize_num(state_max_val)
-  # NOTE: miksi tää kusee jos tähän heittää indiv::Function ????
-  ## --> typeof(indiv) = StaticArraysCore.SVector{32, CANeuron{2, 16}}
-  ## joka ei oo Function, mutta jolle sattumoisin on määritelty (bla::JokuTyyppi)(state). Go fucking figure.
-  function fitness(indiv)
-    result = inp |> Map(_normalize_output ∘ row_from_gray ∘ indiv) |> collect
-    StatsBase.rmsd(result, wanted[idxs])
-  end
-end
-
-
-# HOX: hitaampi ku ei-parallelisoitu versio...
-function _gray_multi_seq_sample_fitness_fn_folds(wanted::T; state_bits::Int=16, sequence_len=3, n_samples=5) where {T<:AbstractArray}
-  start_idx_range = 1:(length(wanted)-sequence_len)
-  max_samples = length(start_idx_range)
-  state_max_val = 2^state_bits - 1
-  _normalize_output = @£ _normalize_num(state_max_val)
-  # NOTE: miksi tää kusee jos tähän heittää indiv::Function ????
-  ## --> typeof(indiv) = StaticArraysCore.SVector{32, CANeuron{2, 16}}
-  ## joka ei oo Function, mutta jolle sattumoisin on määritelty (bla::JokuTyyppi)(state). Go fucking figure.
-  _n_samples = min(max_samples, n_samples)
-  function fitness(indiv)
-    to_ranges = Map(start_idx -> start_idx:start_idx+sequence_len) |> Cat()
-    start_idxs = rand(start_idx_range, _n_samples)
-    idxs = start_idxs |> to_ranges |> collect
-    result = Folds.map(_normalize_output ∘ row_from_gray ∘ indiv ∘ num_to_gray_row, idxs)
-    StatsBase.rmsd(result, wanted[idxs])
-  end
-end
-
-function gray_multi_seq_sample_fitness_fn(wanted::T; state_bits::Int=16, sequence_len=3, n_samples=6) where {T<:AbstractArray}
-  start_idx_range = 1:(length(wanted)-sequence_len)
-  max_samples = length(start_idx_range)
-  state_max_val = 2^state_bits - 1
-  _normalize_output = @£ _normalize_num(state_max_val)
-  # HOX: miksi tää kusee jos tähän heittää indiv::Function ????
-  ## --> typeof(indiv) = StaticArraysCore.SVector{32, CANeuron{2, 16}}
-  ## joka ei oo Function, mutta jolle sattumoisin on määritelty (bla::JokuTyyppi)(state). Go fucking figure.
-  _n_samples = min(max_samples, n_samples)
-  function fitness(indiv)
-    to_ranges = Map(start_idx -> start_idx:start_idx+sequence_len) |> Cat() |> Unique()
-    start_idxs = rand(start_idx_range, _n_samples)
-    idxs = start_idxs |> to_ranges
-    inp = idxs |> Map(num_to_gray_row)
-    result = inp |> Map(_normalize_output ∘ row_from_gray ∘ indiv) |> collect
-    StatsBase.rmsd(result, wanted[idxs|>collect])
-  end
-end
+ =#
 
 ## NOTE: näissä mun objective fn:issä yleensä osa joka ottaa input bitit ja tekee niistä jonkun fenotyypin (tässä tapauksessa 
 ## funktion), ja sit osa joka hieroo sitä training dataa (Map(num_to_gray_row)), ajaa sen sen fenotyypin läpi, ja sit hieroo 
@@ -262,8 +174,8 @@ end
 # end
 
 _test_obj_fn() = to_obj_fn(_test_parser_dynamic(),
-sampled_result_generator(_test_wanted_output(6)),
-Musica.make_fitness_function())
+  sampled_result_generator(_test_wanted_output(6)),
+  Musica.make_fitness_function())
 
 function _do_opt(; f_calls_limit=typemax(Int), time_limit=60 * 0.5, p_mutation=64e-4)
 
