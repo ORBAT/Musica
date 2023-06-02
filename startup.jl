d74b0891a733b57a23eb089f4589f6c8d47b9e4e
Tom Eklöf <tomeklof@gmail.com>
2023-06-02 22:06:44 +0300

    fiksumpi Revise-träkkäys startup-skriptassa

--- startup.jl
+++ startup.jl
@@ -1,15 +1,26 @@
-using TestItems, Test, Transducers, StaticArrays, Revise, BenchmarkTools, Metaheuristics, StatsBase
+using TestItems, Test, Transducers, StaticArrays, Revise, BenchmarkTools, StatsBase, StructArrays
 
 # BenchmarkTools.DEFAULT_PARAMETERS.samples = 30000
 
 includet("src/Musica.jl")
 using .Musica
 
-let prefix = "src/", ext = ".jl", files_to_track = ["CA", "collection_utils", "function_utils", "macros", "Neuron", "optimize", "parsing"]
-  foreach(files_to_track) do filename
-    Revise.track(Musica, prefix * filename * ext)
-  end
+# let prefix = "src/", ext = ".jl", files_to_track = ["CA", "collection_utils", "function_utils", "macros", "Neuron", "optimize", "parsing"]
+#   foreach(files_to_track) do filename
+#     Revise.track(Musica, prefix * filename * ext)
+#   end
+# end
+
+function _track(m::Module, path::String)
+  files = [filename for filename in readdir(path, join=true, sort=false) if splitext(filename)[2] === ".jl"]
+  j = (x) -> join(x, ", ")
+  @info "Telling Revise to track $(j(files)) in module $m"
+  trk = (name) -> begin Revise.track(m, name); name end
+  trk.(files)
 end
+
+_track(Musica, "src/")
+_track(Musica, "src/GA")
 
 function new_state(::Type{Val{L}}) where {L}
   let bla = zeros(Int, L)
