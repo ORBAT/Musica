a7b72c8ae0034f2dd9c55c7c972f9d212f4dba58
Tom Eklöf <tomeklof@gmail.com>
2023-06-09 01:25:56 +0300

    pop methods bla

--- src/GA/population_methods.jl
+++ src/GA/population_methods.jl
@@ -1,7 +1,28 @@
-using StatsBase, Random
+using StatsBase, Random, StructArrays
+using ..Musica: _SizedTypes
 
-function tournament_select(individuals, tournament_size=2)
-  error("WIP")
+"""
+palauttaa 
+    [(parent_a1, parent_b1), (parent_a2, parent_b2), [...], (parent_an, parent_bn)]
+"""
+function select_parents(population::AbstractArray{Individual}, n; tournament_size=3, rng=Random.default_rng())::AbstractArray{NTuple{2,Individual}}
+  # HOX: population pitää olla fitnessin mukaan järjestyksessä, paras eka
+  # TODO FIXME: poista tää assert jahka homma pelittää
+  @assert issorted(population; by=fitness)
+  # (Individual, Individual)[]
+  parents = Array{NTuple{2,Individual}}(undef, n)
+  foreach(1:n) do i
+    @inbounds parents[i] = tournament_select(population, Val{2}; tournament_size, rng)
+  end
+  # map(Tuple, reshape(flat_parents, (2,n)))
+  parents
+end
+
+function tournament_select(individuals::AbstractArray{T}, ::Type{Val{n}}; tournament_size, rng)::NTuple{n,T} where {T,n}
+  # arvotaan `tournament_size` kpl indeksejä `individuals`ista. Ei toistoja, ja järjestyksessä pienin ensin
+  idxs = StatsBase.sample(rng, 1:length(individuals), tournament_size; replace=false, ordered=true)
+  # otetaan näistä indekseistä ekat n (eli parhaat `n` kpl), ja käytetään sitä maskina `individuals`ille --> turnauksen parhaat `n` kpl yksilöä
+  @inbounds Tuple(individuals[idxs[1:n]])
 end
 
 
@@ -12,9 +33,19 @@ end
 #
 ## -------> HUOM: ei haittaa! Tää on täysin agnostic a:n ja b:n rakenteelle. et jos niistä tekee esim Vector{Vector{Bool}} tmv ni homma
 # pelannee mainiosti. TODO: VITTU TESTAA ETTÄ SE PELAA
-function uniformish_crossover(a::AbstractArray, b::AbstractArray; rng=Random.default_rng())
-  # HOX! vanhemmalta saatujen bittien etäisyydet ei sais muuttua, koska geenien linkage (ks essentials of metaheuristics p38)
-  # eli esim jos a:sta otetaan bitit [2, 3, 4, 6], ne on sit jälkeläisessä aina just tossa järjestyksessä, ja esim bitin 4 ja 6 välissä 
+
+
+
+
+function uniform_crossover(a::AbstractArray, b::AbstractArray; rng=Random.default_rng())
+  # FIXME: kommenttien yksityiskohdat on vähän vituillaan ku muokkasin lennossa, vois fiksaa
+
+  # HOX! vanhemmalta saatujen bittien etäisyydet ei sais muuttua, koska epistaasi (ks essentials of metaheuristics p38). 
+  # Se ratkaiseva "geenipatterni" voi olla 
+  #     _ _ 1 _ 0 0 1 _ 0
+  # tmv, eli bittien keskinäinen välimatka merkitsee.
+  # 
+  # eli esim jos a:sta otetaan bitit indekseillä [2, 3, 4, 6], ne on sit jälkeläisessä aina just tossa järjestyksessä, ja esim bitin 4 ja 6 välissä 
   # voi olla vaan 1 bitti b:stä
 
   # a = [10,  20,  30,  40,  50,  60, 70, 80, 90]
@@ -22,19 +53,18 @@ function uniformish_crossover(a::AbstractArray, b::AbstractArray; rng=Random.def
   a_len = length(a)
   # a_len = 9
   b_len = length(b)
-  # b_len = 4
-  # (shorter_len, longer_len) = _minmax(a_len, b_len)
+  # b_len = 5
   (shorter, shorter_len, longer, longer_len) = if a_len < b_len
     (a, a_len, b, b_len)
   else
     (b, b_len, a, a_len)
   end
 
-  # kun toisen roundaa ylös ja toisen alas, niin jos a_len==b_len ja se on pariton, ni offspring_len == a_len == b_len
+  # kun toisen roundaa ylös ja toisen alas, niin jos a_len==b_len ja pariton, ni offspring_len == a_len == b_len
   n_bits_from_shorter = div(shorter_len, 2, RoundUp)
   # n_bits_from_shorter = 3
   n_bits_from_longer = div(longer_len, 2, RoundDown)
-  # n_bits_from_longer = 5
+  # n_bits_from_longer = 4
   offspring_len = n_bits_from_shorter + n_bits_from_longer
   # offspring_len = 8
 
@@ -46,9 +76,7 @@ function uniformish_crossover(a::AbstractArray, b::AbstractArray; rng=Random.def
   # [100, 200, 300, 400, 500]
   #   ^         ^         ^
   #
-  # shorter_mask_len = _mask_len(shorter_mask)
-  # shorter_mask_len = 5
-  # shorterin sapluuna on yhteensä 5 pitkä, eli se mahtuu alkamaan jälkeläisessä indekseiltä
+  # shorterin sapluuna on yhteensä 5 pitkä (kun laskee "tyhjät välit" mukaan), eli se mahtuu alkamaan jälkeläisessä indekseiltä
   # 1: [s, _, s, _, s, _, _, _]
   # 2: [_, s, _, s, _, s, _, _]
   # 3: [_, _, s, _, s, _, s, _]
@@ -59,14 +87,15 @@ function uniformish_crossover(a::AbstractArray, b::AbstractArray; rng=Random.def
 
   # nyt sitä sapluunaa pitää shiftata sen verran että se alkaa halutusta kohdasta.
   #
-  # Nää on sit ne _jälkeläisen_ indeksit jotka tulee shorterilta. Järjestys ja numeroiden suhteellinen paikka säilyy, 
+  # Nää on sit ne _jälkeläisen_ bitti-indeksit jotka tulee shorterilta. Järjestys ja numeroiden suhteellinen paikka säilyy, 
   # shorter_mask vaan siirtyy
   # idxs_from_shorter = shorter_mask .+ shorter_mask_offset
-  idxs_from_shorter = _randomize_mask_position(shorter_mask, offspring_len,rng)
+  idxs_from_shorter = _randomize_mask_position(shorter_mask, offspring_len, rng)
   # idxs_from_shorter = [3, 5, 7]
 
+  # Me tiedetään että ne bitit jotka ei tullu shorterilta, tulee pakostakin sit longerilta. 
   # idxs_from_shorter:in komplementti, eli siis _jälkeläisen_ indeksit jotka tulee longerilta. Tää ottaa rangesta 1:offspring_len kaikki,
-  # jotka ei löydy idxs_from_shorter:ista, ts. ne indeksit jotka ei tuu shorterilta tulee longerilta.
+  # jotka ei löydy idxs_from_shorter:ista.
   idxs_from_longer = findall((!in).(1:offspring_len, (idxs_from_shorter,)))
   # idxs_from_longer = [1, 2, 4, 6, 8]
   #= NOTE:
@@ -81,14 +110,17 @@ function uniformish_crossover(a::AbstractArray, b::AbstractArray; rng=Random.def
   offspring
 end
 
-# HOX: olettaa että `indices` on little-endian järjestyksessä
+"""
+HOX: olettaa että `indices` on little-endian järjestyksessä
+"""
 Base.@propagate_inbounds _mask_len(indices) = (indices[end] - indices[1]) + 1
 
-Base.@propagate_inbounds function _randomize_mask_position(mask, last_index, rng)
+"""
+`mask` shiftattuna satunnaisesti välille `1:max_index`
+"""
+Base.@propagate_inbounds function _randomize_mask_position(mask, max_index, rng)
   mask_len = _mask_len(mask)
-  start_idx = rand(rng, 1:(last_index-mask_len)+1)
+  start_idx = rand(rng, 1:(max_index-mask_len)+1)
   offset = start_idx - mask[1]
   mask .+ offset
 end
-
-@inline _minmax(a, b) = a < b ? (a, b) : (b, a)
