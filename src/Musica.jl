f0c54bd57a0c50bfa3f534a98fbdee3a02d6acdc
Tom Eklöf <tomeklof@gmail.com>
2023-06-30 08:24:05 +0300

    "backport" kluge viewien iteroinnin nopeuttamiseksi

--- src/Musica.jl
+++ src/Musica.jl
@@ -1,4 +1,11 @@
 module Musica
+
+###### HOX FIXME: "backport" julian masterissa olevista muutoksista, ettei viewien iterointi ei oo hidasta.
+if VERSION < v"1.9.2"
+  Base.checkindex(::Type{Bool}, inds::Base.IdentityUnitRange, i::Real) = Base.checkindex(Bool, inds.indices, i)
+  Base.checkindex(::Type{Bool}, inds::Base.OneTo{T}, i::T) where {T<:Base.BitInteger} = unsigned(i - one(i)) < unsigned(last(inds))
+end
+
 include("macros.jl")
 include("collection_utils.jl")
 include("function_utils.jl")
